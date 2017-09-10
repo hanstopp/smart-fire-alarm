c80b6526e6daee777ff0bcbb5848d9ef095d0172
IvanTopp <itopp2015@alu.uct.cl>
2017-09-09 22:23:08 -0300

    Modificación al Modulo HTTP
    Se hacen las modificaciones mencionadas en la retroalimentación realizada.

--- SmartFireAlarm/SmartFireAlarm/station module/Simulacion/httpModule.py
+++ SmartFireAlarm/SmartFireAlarm/station module/Simulacion/httpModule.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-import httplib
+import httplib, json
 class Http():
     def __init__(self, host):
         self.cnx = httplib.HTTPConnection(host)
 
-    def post(self, url, headers, body, callback):
-        self.cnx.request('POST', url, body, headers)
-        callback(self.cnx)
+    def post(self, url, body):
+        self.header = {"Content-type": "application/x-www-form-urlencoded", "Accept": "application/json"}
+        self.cnx.request('POST', url, body, self.header)
         respuesta = self.cnx.getresponse()
         if respuesta.status == 200:
-            return respuesta.read()
+            return json.loads(respuesta.read())
         else:
             return -1
 
