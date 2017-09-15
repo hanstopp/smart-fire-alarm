4c7aeb382a6a416308071d692dde3eff88f79ae6
ElliottMardones <elliottmardones@gmail.com>
2017-09-15 20:34:19 -0300

    avance DOM

--- SmartFireAlarm/SmartFireAlarm/www/js/index.js
+++ SmartFireAlarm/SmartFireAlarm/www/js/index.js
@@ -1,49 +1,64 @@
 var app = {
 	initialize: function() {
-		document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('onDeviceReady', this.onDeviceReady.bind(this), false);
 	},
     onDeviceReady: function () {
         console.log("asdsad");
     },
 
+
 };
 app.initialize();
 
 
-function Login() {
-    $("#navigation-nanvas").hide();
-    $("#mainContainer").hide();
-
-    var inputname = $("<input class='form- control input- lg' type='text' placeholder='usuario' name='user' autofocus><br>");
-    var inputpassword = $("<input class='form- control input- lg' type='password' placeholder='usuario' name='user' required>");
-    var inputsubmit = $("<input type='submit' name='submit'>");
-    //newP.text("pericos cocodrilos ");
-    
-    $("body").append(inputname,inputpassword,inputsubmit);
-}
-
-
-function Login1(){
-    
-    $('#desconectar,#ingresar').click(function(){
-
-        $('#mainContainer').hide();
-        $('#copyr').hide();
-        $('#contenedor').fadeIn(1000);
-        
-    });
-
-    $('#inicio').click(function(){
-        $('#contenedor').hide();
-        $('#mainContainer').fadeIn(1000);
-        $('#copyr').fadeIn(1000);
-    });
-}
-/*
-document.getElementById("inicio").addEventListener("click", function () {
-    document.getElementById("logo").innerHTML = "Hello World";
+$(document).ready(function () {
+    $('#conf_form').hide();
+    $('#Cuenta_form').hide();
+    $('#Login').hide();
 });
 
-*/
 
 
+function Inicio() {
+    $('#Inicio').click(function () {
+        $('#conf_form').hide();
+        $('#Cuenta_form').hide();
+        $('#Login').hide();
+        $('#Alertas_form').show();
+    });
+}
+
+function Configuracion() {
+    $('#Configuracion').click(function () {
+        $('#Alertas_form').hide();
+        $('#Cuenta_form').hide();
+        $('#Login').hide();
+        $('#conf_form').show();
+    });
+}
+
+function Cuenta() {
+    $('#Cuenta').click(function () {
+        $('#Alertas_form').hide();
+        $('#conf_form').hide();
+        $('#Login').hide();
+        $('#Cuenta_form').show();
+        
+    });
+}
+
+function Desconectar() {
+    $('#Desconectar').click(function () {
+        $('#navigation-nanvas').hide();
+        $('#Alertas_form').hide();        
+        $('#conf_form').hide();
+        $('#Cuenta_form').hide();
+        $('#Login').show();
+    });
+}
+
+
+function Login() {
+
+
+}
