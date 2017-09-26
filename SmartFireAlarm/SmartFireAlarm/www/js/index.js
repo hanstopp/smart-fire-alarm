268df322a723ff54c34d9b99377ee0dda76fd2cb
hanstopp <htopp2016@alu.uct.cl>
2017-09-25 22:25:36 -0300

    Update index.js
    Alertas (2)

--- SmartFireAlarm/SmartFireAlarm/www/js/index.js
+++ SmartFireAlarm/SmartFireAlarm/www/js/index.js
@@ -15,8 +15,8 @@ $(document).ready(function () {
     $('#conf_form').hide();
     $('#Cuenta_form').hide();
     $('#Login').hide();
+    $('#Alertas_form').hide();
 });
-
 
 
 function Inicio() {
@@ -62,6 +62,27 @@ function Login() {
 
 
 }
+
+function Alertas(){
+    var text = '{ "incident" : [' +
+                '{ "code":"01" , "date":"24-09-2017", "time":"12:54:02" , "user":"perico", "sensorData":"value1" },' +
+                '{ "code":"02" , "date":"20-09-2017", "time":"22:31:14" , "user":"juan"  , "sensorData":"value2" },' +
+                '{ "code":"03" , "date":"13-09-2017", "time":"15:41:39" , "user":"carlos", "sensorData":"value3" } ]}';
+    var obj = JSON.parse(text);
+    document.write("<table border='1'><tr>");
+    document.write("<td> CODE </td><td> DATE </td><td> TIME </td><td> USER </td><td> SENSOR_DATA </td></tr>");
+    for (var i=0; i<100; i++){
+        document.write("<tr>")
+        document.write("<td>"+obj.incident[i].code+"</td>");
+        document.write("<td>"+obj.incident[i].date+"</td>");
+        document.write("<td>"+obj.incident[i].time+"</td>");
+        document.write("<td>"+obj.incident[i].user+"</td>");
+        document.write("<td>"+obj.incident[i].sensorData+"</td>");
+        document.write("</tr>")
+        }
+    document.write("</table>");    
+}
+
 /*
 no tomar en cuenta
 function selectRedes() {
